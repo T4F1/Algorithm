f062fdade405b97c50ffd27c68fddc3ec0847abf
Jewan Baek <116615068+Jewan1120@users.noreply.github.com>
2025-02-26 11:28:14 +0900

    백제완: 5639 이진 검색 트리

---
+++ 백준/5639/JW_5639.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int[] tree = new int[10_001];
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+        int n = 0;
+        while (!(str = br.readLine()).equals(""))
+            tree[n++] = Integer.parseInt(str);
+        postOrder(0, n);
+        System.out.println(sb);
+    }
+
+    private static void postOrder(int s, int e) {
+        if (s >= e)
+            return;
+        int m = findSplit(s, e, tree[s]);
+        postOrder(s + 1, m);
+        postOrder(m, e);
+        sb.append(tree[s]).append("\n");
+    }
+
+    private static int findSplit(int l, int r, int target) {
+        int m = l + 1;
+        while (m < r && tree[m] < target) {
+            m++;
+        }
+        return m;
+    }
+}
